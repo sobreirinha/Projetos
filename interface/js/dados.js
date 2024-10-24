541fafb4cbe47b0f4038eb828943a3684b6e27a3
Gustavo Sobreira <137525185+sobreirinha@users.noreply.github.com>
2024-10-24 11:23:56 -0300

    Atualizando interface
    acrescentando informações.

--- interface/js/dados.js
+++ interface/js/dados.js
@@ -1,7 +1,7 @@
 const projetos = [
    {
       imagem: "imagens/projeto_mundo_movie.gif",
-      url: "https://sobreirinha.github.io/projetos/projeto-mundo-movie/html/mundo_movie.html",
+      url: "https://sobreirinha.github.io/projetos/projeto_mundo_movie/html/mundo_movie.html",
       nomeProjeto: "Mundo Movie",
       linguagem: "JavaScript, CSS, HTML",
       descricao: "Site de filmes utilizado uma API simples."
@@ -15,7 +15,7 @@ const projetos = [
    },
    {
       imagem: "imagens/projeto_minecraf_clone.gif",
-      url: "https://sobreirinha.github.io/projetos/login_minecraft/index.html",
+      url: "https://sobreirinha.github.io/projetos/projeto_clone_minecraft/index.html",
       nomeProjeto: "Clone Minecraft",
       linguagem: "HTML, CSS",
       descricao: "Projeto de clone da interface do Minecraft"
@@ -36,31 +36,38 @@ const projetos = [
    },
    {
       imagem: "imagens/projeto_redes_sociais_frame.gif",
-      url: "https://sobreirinha.github.io/projetos/projeto-rede-social/index.html",
+      url: "https://sobreirinha.github.io/projetos/projeto_rede_social/index.html",
       nomeProjeto: "Projeto Rede Social",
       linguagem: "HTML, CSS",
       descricao: "Usando iframe para incorporar outra página HTML dentro da página atual."
    },
    {
       imagem: "imagens/projeto_primeiro_site.gif",
-      url: "https://sobreirinha.github.io/projetos/Primeiro%20Site/site1.html",
+      url: "https://sobreirinha.github.io/projetos/projeto_primeiro_site/site1.html",
       nomeProjeto: "Primeiro site",
       linguagem: "HTML, CSS, JS",
       descricao: "Primeiro site desenvolvimeto por mim."
    },
    {
       imagem: "imagens/projeto_rbtech.gif",
-      url: "https://sobreirinha.github.io/projetos/RBtech/index.html",
+      url: "https://sobreirinha.github.io/projetos/projeto_rbtech/index.html",
       nomeProjeto: "Projeto do canal RBtech",
       linguagem: "HTML, CSS",
       descricao: "Desenvolvendo um site simples."
    },
    {
       imagem: "imagens/projeto_imc.gif",
-      url: "https://sobreirinha.github.io/projetos/projeto-imc/imc.html",
+      url: "https://sobreirinha.github.io/projetos/projeto_imc/imc.html",
       nomeProjeto: "Calculadora - IMC",
       linguagem: "JS, HTML, CSS",
       descricao: "Calcular IMC do usúario."
+   },
+   {
+      imagem: "imagens/projeto_tshirt_store.gif",
+      url: "https://sobreirinha.github.io/projetos/projeto_tshirt_store/t-shirt_store.html",
+      nomeProjeto: "T-shirt Store",
+      linguagem: "JS, HTML, CSS",
+      descricao: "Aplicando modo escuro."
    }
 ];
 
